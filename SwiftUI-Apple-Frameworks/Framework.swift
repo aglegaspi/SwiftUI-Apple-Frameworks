3dd6bda59ac5ee65a035d425bdf7e5ed991d9901
aglegaspi <lex83nyc@gmail.com>
2021-01-12 20:47:02 -0500

    set up images assets and model

---
+++ SwiftUI-Apple-Frameworks/Framework.swift
@@ -0,0 +1,17 @@
+//
+//  Framework.swift
+//  SwiftUI-Apple-Frameworks
+//
+//  Created by Alex 6.1 on 1/12/21.
+//
+
+import Foundation
+
+struct Framework {
+    let name: String
+    let imageName: String
+    let urlString: String
+    let description: String
+    
+    
+}
---
+++ SwiftUI-Apple-Frameworks/FrameworkGridView.swift
@@ -0,0 +1,20 @@
+//
+//  FrameworkGridView.swift
+//  SwiftUI-Apple-Frameworks
+//
+//  Created by Alex 6.1 on 1/12/21.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FrameworkGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworkGridView()
+    }
+}
