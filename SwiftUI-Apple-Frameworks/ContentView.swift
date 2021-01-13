a3dafc464bbe25923ca65b0f50b37cb64d3f90e1
aglegaspi <lex83nyc@gmail.com>
2021-01-12 20:35:04 -0500

    Initial Commit

---
+++ SwiftUI-Apple-Frameworks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Apple-Frameworks
+//
+//  Created by Alex 6.1 on 1/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Apple-Frameworks/SwiftUI_Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Apple_FrameworksApp.swift
+//  SwiftUI-Apple-Frameworks
+//
+//  Created by Alex 6.1 on 1/12/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
