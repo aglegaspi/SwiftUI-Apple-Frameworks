8921406c5b5d5f7a11d52b8baf76e268c5493a8d
aglegaspi <lex83nyc@gmail.com>
2021-01-13 21:40:51 -0500

    set up FrameTitleView

--- SwiftUI-Apple-Frameworks/FrameworkGridView.swift
+++ SwiftUI-Apple-Frameworks/FrameworkGridView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct FrameworkGridView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        FrameworkTitleView(name: "App Clips", imageName: "app-clip")
     }
 }
 
@@ -17,4 +17,25 @@ struct FrameworkGridView_Previews: PreviewProvider {
     static var previews: some View {
         FrameworkGridView()
     }
+}
+
+struct FrameworkTitleView: View {
+    let name: String
+    let imageName: String
+    
+    var body: some View {
+        VStack {
+            Image(name)
+                .resizable()
+                .frame(width: 90,height: 90)
+            Text(imageName)
+                .font(.title3)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.6)
+        }
+        
+    }
+    
+    
 }
