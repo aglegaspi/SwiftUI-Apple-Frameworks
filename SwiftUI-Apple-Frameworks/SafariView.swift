15cee7b3c78ab6d2f33d6e49fe433039d777dbfe
aglegaspi <lex83nyc@gmail.com>
2021-01-16 23:58:21 -0500

    set up SafariView

---
+++ SwiftUI-Apple-Frameworks/SafariView.swift
@@ -0,0 +1,20 @@
+//
+//  SafariView.swift
+//  SwiftUI-Apple-Frameworks
+//
+//  Created by Alex 6.1 on 1/16/21.
+//
+
+import SwiftUI
+import SafariServices
+
+struct SafariView: UIViewControllerRepresentable {
+    
+    let url: URL
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<SafariView>) -> SFSafariViewController {
+        SFSafariViewController(url: url)
+    }
+    
+    func updateUIViewController(_ uiViewController: SFSafariViewController, context: UIViewControllerRepresentableContext<SafariView>) { }
+}
